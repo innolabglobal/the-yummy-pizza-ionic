4b19cd0098347711f7226cc715383cc79ba3e612
JY Liu <ljieyao0210@gmail.com>
2020-05-01 23:29:32 +0800

    implement menu.page

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  menuList = [];
+
+  constructor(private menuService: MenuService) { }
 
   ngOnInit() {
+    this.menuService.getAll().subscribe(res => this.menuList = res);
   }
 
 }
