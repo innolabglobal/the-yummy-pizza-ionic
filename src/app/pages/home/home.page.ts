261547181d2a5abfd98abf58cdf4d2fa37938778
JY Liu <ljieyao0210@gmail.com>
2020-05-02 23:25:31 +0800

    add basic content to home.page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+
+const SLIDES = [
+  {
+    image: 'assets/images/home-slider-1.jpg',
+    title: 'Do you like Pizza?'
+  },
+  {
+    image: 'assets/images/home-slider-2.jpg',
+    title: 'We use fresh ingredient'
+  },
+  {
+    image: 'assets/images/home-slider-3.jpg',
+    title: 'Various type of Pizza'
+  },
+  {
+    image: 'assets/images/home-slider-4.jpg',
+    title: 'Our Pizza is Yummy!'
+  },
+  {
+    image: 'assets/images/home-slider-5.jpg',
+    title: 'Enjoy The Yummy Pizza!'
+  },
+];
 
 @Component({
   selector: 'app-home',
@@ -7,9 +30,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  sliderOptions = {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+  };
+  slides = SLIDES;
+
+  constructor(public renderer: Renderer2) { }
 
   ngOnInit() {
   }
 
+  setBackgroundImage(slide, elem) {
+    this.renderer.setStyle(elem.el, 'background', `linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url(${slide.image})`);
+  }
 }
