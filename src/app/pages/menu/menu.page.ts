eebb5e5dd78a99572c5176ffd24e1559a950fea4
JY Liu <ljieyao0210@gmail.com>
2020-05-03 00:47:01 +0800

    add cart btn to menubar

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../../services/menu.service';
+import { OnViewWillEnter } from '../../models/ion-lifecycle';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
-export class MenuPage implements OnInit {
+export class MenuPage implements OnInit, OnViewWillEnter {
 
+  savedCart = [];
   menuList = [];
 
-  constructor(private menuService: MenuService) { }
+  constructor(private cartService: CartService,
+              private menuService: MenuService) { }
 
   ngOnInit() {
     this.menuService.getAllMenu().subscribe(res => this.menuList = res);
   }
 
+  ionViewWillEnter() {
+    this.cartService.getCart();
+    this.savedCart = this.cartService.cart;
+  }
 }
