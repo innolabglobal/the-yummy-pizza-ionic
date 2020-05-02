b6f2bf9a24300801ca0108e6462716cacb9bcce9
JY Liu <ljieyao0210@gmail.com>
2020-05-02 23:23:27 +0800

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,12 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
   providers: [
     StatusBar,
     SplashScreen,
