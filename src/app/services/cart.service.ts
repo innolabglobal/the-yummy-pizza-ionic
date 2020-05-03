e14e55984f89739f2b457e17451444b118402f71
JY Liu <ljieyao0210@gmail.com>
2020-05-03 12:37:19 +0800

    add function for cart.service
    1. addItemQuantity()
    2. removeItemQuantity()
    3. removeItem()
    4. recalculateTotalPrice()

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -15,8 +15,64 @@ export class CartService {
     this.getCart();
   }
 
+  addItemQuantity(data) {
+    this.getCart();
+
+    if (data.item.itemQuantity < 10) {
+      data.item.itemQuantity = data.item.itemQuantity + 1;
+
+      this.recalculateTotalPrice(data);
+
+      localStorage.setItem(KEY_CART, JSON.stringify(this.cart));
+    }
+  }
+
   getCart() {
     this.cart = JSON.parse(localStorage.getItem(KEY_CART));
+  }
+
+  recalculateTotalPrice(data) {
+    this.cart.map(cartItem => {
+      let totalPrice = 0;
+
+      if (cartItem.item.itemId === data.item.itemId &&
+        cartItem.item.price.pname === data.item.price.pname) {
+        cartItem.item.itemQuantity = data.item.itemQuantity;
+        totalPrice = cartItem.item.price.value;
+
+        cartItem.itemTotalPrice = totalPrice * data.item.itemQuantity;
+
+        return cartItem;
+      }
+    });
+  }
+
+  removeItem(data) {
+    for (let i = 0; i < this.cart.length; i++) {
+      if (this.cart[i].item.itemId === data.item.itemId &&
+        this.cart[i].item.price.pname === data.item.price.pname) {
+        this.cart.splice(i, 1);
+
+        if (this.cart.length === 0) {
+          localStorage.removeItem(KEY_CART);
+        } else {
+          localStorage.setItem(KEY_CART, JSON.stringify(this.cart));
+          this.cart = JSON.parse(localStorage.getItem(KEY_CART));
+        }
+      }
+    }
+  }
+
+  removeItemQuantity(data) {
+    this.getCart();
+
+    if (data.item.itemQuantity > 1) {
+      data.item.itemQuantity = data.item.itemQuantity - 1;
+
+      this.recalculateTotalPrice(data);
+
+      localStorage.setItem(KEY_CART, JSON.stringify(this.cart));
+    }
   }
 
   saveCart(item: any) {
