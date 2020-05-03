761f08b7d1fb6158e4b8575bf4a96a4ad8679917
JY Liu <ljieyao0210@gmail.com>
2020-05-03 13:29:30 +0800

    create address.service

---
+++ src/app/services/address.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+const DUMMY_ADDRESS_LIST = [
+  {
+    address: 'Knesebeckstraße 75',
+    area: 'Schleswig-Holstein',
+    city: 'Witzhave',
+    mobileNo: '02532753469',
+    name: 'Parent Home',
+    postcode: '48346'
+  },
+  {
+    address: '1058  Mapleview Drive',
+    area: 'Schleswig-Holstein',
+    city: 'Münsterappel',
+    mobileNo: '02532753469',
+    name: 'Home',
+    postcode: '48346'
+  },
+  {
+    address: '4535  Larry Street',
+    area: 'Schleswig-Holstein',
+    city: 'Ostbevern',
+    mobileNo: '02532753469',
+    name: 'Work',
+    postcode: '48346'
+  }
+];
+
+const DUMMY_DELIVERABLE_POSTCODE = [
+  { postcode: 48346 },
+  { postcode: 56075 },
+  { postcode: 57401 },
+  { postcode: 57476 },
+  { postcode: 57146 },
+  { postcode: 57125 },
+  { postcode: 57480 },
+  { postcode: 57101 },
+  { postcode: 78900 },
+  { postcode: 57101 },
+  { postcode: 57475 },
+  { postcode: 67895 },
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+
+  constructor() { }
+
+  getAddressList() {
+    return of(DUMMY_ADDRESS_LIST);
+  }
+
+  getDeliverablePostcode() {
+    return of(DUMMY_DELIVERABLE_POSTCODE);
+  }
+}
