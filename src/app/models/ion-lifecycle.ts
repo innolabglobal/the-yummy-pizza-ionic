aaa09edd625acb7b79067f8816b67594f0daa23b
JY Liu <ljieyao0210@gmail.com>
2020-05-03 14:14:39 +0800

    create ion-lifecycle interface

---
+++ src/app/models/ion-lifecycle.ts
@@ -0,0 +1,15 @@
+export declare interface OnViewWillEnter {
+  ionViewWillEnter(): void;
+}
+
+export declare interface OnViewDidEnter {
+  ionViewDidEnter(): void;
+}
+
+export declare interface OnViewWillLeave {
+  ionViewWillLeave(): void;
+}
+
+export declare interface OnViewDidLeave {
+  ionViewDidLeave(): void;
+}
