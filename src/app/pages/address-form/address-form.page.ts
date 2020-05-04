0d02b036d3533351daf4ed707bd374608dc03da8
JY Liu <ljieyao0210@gmail.com>
2020-05-04 14:13:49 +0800

    fix the routing login when success addAddress()

--- src/app/pages/address-form/address-form.page.ts
+++ src/app/pages/address-form/address-form.page.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AddressService } from '../../services/address.service';
+import { AlertController, NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { OnViewWillEnter } from '../../interfaces/ion-lifecycle.interface';
 
 @Component({
   selector: 'app-address-form',
   templateUrl: './address-form.page.html',
   styleUrls: ['./address-form.page.scss'],
 })
-export class AddressFormPage implements OnInit {
+export class AddressFormPage implements OnInit, OnViewWillEnter {
 
   address = {
     name: '',
@@ -16,14 +19,34 @@ export class AddressFormPage implements OnInit {
     mobileNo: '',
     address: ''
   };
+  orderDetails: any = {};
 
-  constructor(public addressService: AddressService) { }
+  constructor(public activatedRoute: ActivatedRoute,
+              public addressService: AddressService,
+              public alertCtrl: AlertController,
+              public navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
-  addAddress() {
-    console.log(this.address);
-    this.addressService.addLocalAddress(this.address);
+  ionViewWillEnter() {
+    this.orderDetails = { ...this.activatedRoute.snapshot.queryParams };
+  }
+
+  async addAddress() {
+    await this.addressService.addLocalAddress(this.address);
+
+    const alert = await this.alertCtrl.create({
+      header: 'Success',
+      subHeader: 'You have successfully added a new address. Please proceed now.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => this.navCtrl.navigateBack('/tabs/address-list', { queryParams: this.orderDetails })
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
