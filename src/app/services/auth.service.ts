583d6f539610bbc1013152b4b0006ccc5846fe82
JY Liu <ljieyao0210@gmail.com>
2020-05-06 22:17:59 +0800

    create login() and logout() in auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { tap } from 'rxjs/operators';
+import { ApiResponseInterface } from '../interfaces/api-response.interface';
+
+const TOKEN_STORAGE_KEY = 'ACCESS_TOKEN';
+const PROFILE_STORAGE_KEY = 'USER_PROFILE';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  init() {
+    return this.http
+      .get(`${environment.apiBaseUrl}/sanctum/csrf-cookie`, { observe: 'response' });
+  }
+
+  getOrderHistory() {
+    return this.http.get(`${environment.apiBaseUrl}/api/orders`);
+  }
 
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('uid');
+    return !!localStorage.getItem(TOKEN_STORAGE_KEY);
+  }
+
+  login(credential) {
+    return this.http
+      .post(`${environment.apiBaseUrl}/api/login`, credential)
+      .pipe(
+        tap(((res: ApiResponseInterface) => {
+          localStorage.setItem(TOKEN_STORAGE_KEY, res.data.token);
+          localStorage.setItem(PROFILE_STORAGE_KEY, JSON.stringify({
+            id: res.data.id,
+            name: res.data.name,
+            email: res.data.email,
+          }));
+        }))
+      );
+  }
+
+  logout() {
+    return this.http
+      .post(`${environment.apiBaseUrl}/api/logout`, {})
+      .pipe(
+        tap(((res: ApiResponseInterface) => {
+          localStorage.removeItem(TOKEN_STORAGE_KEY);
+          localStorage.removeItem(PROFILE_STORAGE_KEY);
+        }))
+      );
+  }
+
+  profile() {
+    return localStorage.getItem(PROFILE_STORAGE_KEY);
+  }
+
+  token() {
+    return localStorage.getItem(TOKEN_STORAGE_KEY);
   }
 }
