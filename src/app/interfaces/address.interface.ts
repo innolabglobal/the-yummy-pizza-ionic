ac60e24bbf4d549d96b2cbc71b18d6d03419fc08
JY Liu <ljieyao0210@gmail.com>
2020-05-06 23:59:53 +0800

    create address.interface

---
+++ src/app/interfaces/address.interface.ts
@@ -0,0 +1,11 @@
+export interface AddressInterface {
+  id: number;
+  name: string;
+  first_name: string;
+  last_name: string;
+  address: string;
+  city: string;
+  post_code: string;
+  phone_number: string;
+  user_id: number;
+}
