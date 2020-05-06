70c611314b68d7d7402edd6b5fe2758c35d4cd41
JY Liu <ljieyao0210@gmail.com>
2020-05-06 23:15:32 +0800

    refactor to add response interface into oreder.service

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
+import { ApiResponseInterface } from '../interfaces/api-response.interface';
+import { Observable } from 'rxjs';
+import { OrderInterface } from '../interfaces/order.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +13,15 @@ export class OrderService {
 
   constructor(private http: HttpClient) { }
 
-  getOrder(id) {
+  getOrder(id): Observable<OrderInterface> {
     return this.http
       .get(`${environment.apiBaseUrl}/api/own/orders/${id}`)
-      .pipe(map(res => res.data));
+      .pipe(map((res: ApiResponseInterface) => res.data));
   }
 
-  getOrders() {
+  getOrders(): Observable<OrderInterface[]> {
     return this.http
       .get(`${environment.apiBaseUrl}/api/own/orders`)
-      .pipe(map(res => res.data));
+      .pipe(map((res: ApiResponseInterface) => res.data));
   }
 }
