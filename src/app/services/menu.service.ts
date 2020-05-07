4b8cd600a659e33858cb796344b0c0f7029f57bb
JY Liu <ljieyao0210@gmail.com>
2020-05-07 14:11:43 +0800

    code cleanup for menu.service

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
+import { ApiResponseInterface } from '../interfaces/api-response.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,14 @@ export class MenuService {
   constructor(public http: HttpClient) { }
 
   getMenu(id: number) {
-    return this.http.get(`${environment.apiBaseUrl}/api/menus/${id}`).pipe(map(res => res.data));
+    return this.http
+      .get(`${environment.apiBaseUrl}/api/menus/${id}`)
+      .pipe(map((res: ApiResponseInterface) => res.data));
   }
 
   getAllMenu() {
-    return this.http.get(`${environment.apiBaseUrl}/api/menus`).pipe(map(res => res.data));
+    return this.http
+      .get(`${environment.apiBaseUrl}/api/menus`)
+      .pipe(map((res: ApiResponseInterface) => res.data));
   }
 }
